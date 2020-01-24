5c72f0d038f3024c20ae0a40c8e627bab33f5da0
Tomasz Lewicki <t.w.lewicki@gmail.com>
2020-01-24 10:20:34 -0800

    refactored code to get ready for multiprocessing support

--- sim.py
+++ sim.py
@@ -2,6 +2,7 @@ import numpy as np
 import scipy.signal
 import matplotlib.pyplot as plt
 import skimage.io
+import numpy as np
 import os
 import cv2
 
@@ -14,7 +15,8 @@ def ignite_center(state):
     
 
 def gkern(l=5, sig=1.):
-    """    creates gaussian kernel with side length l and a sigma of sig
+    """
+    creates gaussian kernel with side length l and a sigma of sig
     """
 
     ax = np.linspace(-(l - 1) / 2., (l - 1) / 2., l)
@@ -24,72 +26,81 @@ def gkern(l=5, sig=1.):
 
     return kernel / np.sum(kernel)
 
-CELLS = 10000
-TREE_DENSITY = 0.52512
-BURN_RATE = 2 # 0-255
-N_STEPS = 10
-IGNITION_PROB = 0.20
-N_EPOCHS = 10
-COLOR = False
+def save_images(states_history, fuel_history, dir_name, start_number, img_shape= (2000,2000), color=False): #TODO: have corresponding dict {state='blue', fuel='red'}
+    n_images = len(states_history)
+    for i in range(n_images):
 
-n_trees = int(CELLS**2 * TREE_DENSITY) # fill 55% with trees. With 3x3 kernel the fire doesn't spread at 50% rate
+        padded_n = '{0:05d}'.format(start_number*n_images+i)
 
-trees = np.random.randint(0,CELLS,(n_trees,2)) # choose 50000 tree locations (assign fuel to half of cells)
-fuel = np.zeros((CELLS,CELLS), dtype=np.uint8) # could be 1000x1000m
-fuel[trees[:,0], trees[:,1]] = np.random.randint(0,255,n_trees)
-
-# state = np.array(np.zeros_like(fuel), dtype=np.bool)
-# ignite_center(state)
-
-kernel3by3 = np.array([[1,1,1],[1,0,1],[1,1,1]]) # 3x3
-kernel5by5 = 15 *gkern()
-kernel5by5[2,2] = 0
-kernel5by5 = kernel3by3
-
-state = np.zeros_like(fuel)
-ignite_center(state)
-
-states_history = np.array(np.zeros((N_STEPS,*state.shape)), dtype=np.uint8)
-ignitions_history = np.zeros((N_STEPS,*state.shape), dtype=np.uint8)
-fuel_history = np.zeros((N_STEPS,*state.shape), dtype=np.uint8)
-
-name =  f'cells={str(CELLS)}steps={str(N_EPOCHS*N_STEPS)}ignition_prob={str(IGNITION_PROB)}tree_density={str(TREE_DENSITY)}burn_rate={str(BURN_RATE)}'
-os.mkdir(name)
-
-for e in range(N_EPOCHS):
-    print(f'epoch:{e}')
-    # run simulation
-    for i in range(N_STEPS):
-
-
-        # calculate new ignitons
-        count_neighbors_on_fire = scipy.signal.convolve2d(state, kernel5by5, 'same')
-        ignitions = (count_neighbors_on_fire * np.random.random(state.shape) > IGNITION_PROB) * fuel
-        state += ignitions
-        
-
-        #update fuel status
-        on_fire_mask = state > 0
-        burned_out_mask = fuel < BURN_RATE
-        fuel[on_fire_mask] -= BURN_RATE 
-        fuel[burned_out_mask] = 0
-        state[burned_out_mask] = 0
-
-        # update histories
-        ignitions_history[i] = ignitions
-        states_history[i] = state
-        fuel_history[i] = fuel
-
-    # save images
-    for i in range(N_STEPS):
-        padded_n = '{0:05d}'.format(e*N_STEPS+i)
-
-        red = cv2.resize(states_history[i], (2000,2000))
-        if COLOR:
-            green = cv2.resize(fuel_history[i], (2000,2000))
+        red = cv2.resize(states_history[i], img_shape)
+        if color:
+            green = cv2.resize(fuel_history[i], img_shape)
             blue = np.zeros_like(green)
             im = np.stack([red, green, blue], axis=-1)
         else:
             im = red
+        
+        skimage.io.imsave(f'{dir_name}/frame{padded_n}.png', im)
+    
 
-        # skimage.io.imsave(f'{name}/frame{padded_n}.png', im)
+
+kernel3by3 = np.array([[1,1,1],[1,0,1],[1,1,1]]) # 3x3
+kernel5by5 = 15 * gkern()
+kernel5by5[2,2] = 0
+kernel5by5 = kernel3by3
+
+
+def run(n_cells=1000, tree_density=0.525123333333333333333, burn_rate=3, n_steps=10, ignition_prob=0.2, n_epochs=10, save=False, color=False):
+    # initialization n_cells x n_cells could correspond to 1000x1000m
+
+    if save:
+        # we will output images here
+        dir_name =  f'cells={str(n_cells)}steps={str(n_epochs*n_steps)}ignition_prob={str(ignition_prob)}tree_density={str(tree_density)}, burn_rate={str(burn_rate)}'
+        os.mkdir(dir_name)
+
+    # fuel corresponds to the ammount of fuel in each cell (0-255)
+    fuel = np.zeros((n_cells, n_cells), dtype=np.uint8)
+
+    # fill tree_density percentage of cells with trees
+    # (e.g. 0.55 corresponds to 55% cells having trees (fuel) and 45% being empty)
+    n_trees = int(n_cells**2 * tree_density) 
+    trees = np.random.randint(0 ,n_cells, (n_trees,2))
+    fuel[trees[:,0], trees[:,1]] = np.random.randint(0,255,n_trees)
+
+
+    state = np.zeros_like(fuel) # should the type be dtype=np.bool?
+    ignite_center(state)
+    states_history = np.array(np.zeros((n_steps,*state.shape)), dtype=np.uint8)
+    ignitions_history = np.zeros((n_steps,*state.shape), dtype=np.uint8)
+    fuel_history = np.zeros((n_steps,*state.shape), dtype=np.uint8)
+
+
+    for e in range(n_epochs):
+        print(f'epoch:{e}')
+        # run simulation
+        for i in range(n_steps):
+
+
+            # calculate new ignitons
+            count_neighbors_on_fire = scipy.signal.convolve2d(state, kernel5by5, 'same')
+            ignitions = (count_neighbors_on_fire * np.random.random(state.shape) > ignition_prob) * fuel
+            state += ignitions
+            
+
+            #update fuel status
+            on_fire_mask = state > 0
+            burned_out_mask = fuel < burn_rate
+            fuel[on_fire_mask] -= burn_rate 
+            fuel[burned_out_mask] = 0
+            state[burned_out_mask] = 0
+
+            # update histories
+            ignitions_history[i] = ignitions
+            states_history[i] = state
+            fuel_history[i] = fuel
+
+        if save:
+            save_images(states_history, fuel_history, dir_name, start_number=e)
+
+if __name__ == '__main__':
+    run(n_cells=1000, tree_density=0.525, burn_rate=3, n_steps=10, ignition_prob=0.2, n_epochs=10, save=False, color=False)
